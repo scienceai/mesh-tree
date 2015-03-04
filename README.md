49c004ef2a78d8e23d1428d8525d30e9986569f0
L. Leon Chen <lchen3@gmail.com>
2015-03-04 12:02:55 -0500

    basic data extraction for MeSH descriptor records

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 mesh-tree
 ========
 
-An hash of the Medical Subject Heading (MeSH) tree.
+Utility functions for traversing the Medical Subject Heading (MeSH) tree
+
+## Fetch data
 
 Raw data are available
-[here](https://www.nlm.nih.gov/mesh/filelist.html). See the
+[here](https://www.nlm.nih.gov/mesh/filelist.html) as ascii and XML formats. Registration is required: see the
 [Memorandum of Understanding](https://www.nlm.nih.gov/mesh/2014/download/termscon.html).
+
+Run `./bin/fetch_mesh.sh`. The script will prompt for the email used in registration.
+
+## Initialize DB
+
+Run `npm run init` to load data into levelDB. The MeSH XML file is streamed, processed, and loaded into the levelDB key-value store.
---
+++ bin/fetch_mesh.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+mkdir -p ./data
+
+echo "Fetch MeSH files"
+echo "/////////////////////////////////////////"
+echo "Enter email used to register: "
+read REGISTRATION_EMAIL
+if [[ "$REGISTRATION_EMAIL" =~ [A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4} ]]
+then
+
+    echo "/////////////////////////////////////////"
+    echo "Starting download..."
+
+    # Descriptor Records
+    echo "    >>> Descriptor Records <<<"
+    curl -u anonymous:$REGISTRATION_EMAIL -o ./data/mesh_desc_2015.xml ftp://nlmpubs.nlm.nih.gov/online/mesh/.xmlmesh/desc2015.xml
+    curl -u anonymous:$REGISTRATION_EMAIL -o ./data/mesh_desc_2015.dtd http://www.nlm.nih.gov/mesh/2015/download/desc2015.dtd
+    # Qualifier Records
+    echo "    >>> Qualifier Records <<<"
+    curl -u anonymous:$REGISTRATION_EMAIL -o ./data/mesh_qual_2015.xml ftp://nlmpubs.nlm.nih.gov/online/mesh/.xmlmesh/qual2015.xml
+    curl -u anonymous:$REGISTRATION_EMAIL -o ./data/mesh_desc_2015.dtd http://www.nlm.nih.gov/mesh/2015/download/qual2015.dtd
+    # Supplementary Concept Records
+    echo "    >>> Supplementary Concept Records <<<"
+    curl -u anonymous:$REGISTRATION_EMAIL -o ./data/mesh_supp_2015.xml ftp://nlmpubs.nlm.nih.gov/online/mesh/.xmlmesh/supp2015.xml
+    curl -u anonymous:$REGISTRATION_EMAIL -o ./data/mesh_desc_2015.dtd http://www.nlm.nih.gov/mesh/2015/download/supp2015.dtd
+
+else
+    echo "  Invalid email, exiting."
+    exit 1
+fi
