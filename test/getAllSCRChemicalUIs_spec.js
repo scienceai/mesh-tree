3a0f9ca291f620684c15a673217dfb59d5786b43
Leon Chen <lchen3@gmail.com>
2015-09-13 14:50:58 -0400

    fix travis test

--- test/getAllSCRChemicalUIs_spec.js
+++ test/getAllSCRChemicalUIs_spec.js
@@ -5,7 +5,7 @@ describe('get array of all chemical supplement record UIs', function () {
   var allChemUIs;
 
   before(function (done) {
-    this.timeout(15000);
+    this.timeout(60000);
     meshTree.getAllSCRChemicalUIs().then(function (result) {
       allChemUIs = result;
       done();
@@ -14,12 +14,12 @@ describe('get array of all chemical supplement record UIs', function () {
 
   it('should return an array', function () {
     expect(allChemUIs).to.be.instanceOf(Array);
-  })
+  });
   it('should contain 222666 elements', function () {
     expect(allChemUIs.length).to.eql(222666);
-  })
+  });
   it('should include C025735', function () {
     expect(allChemUIs).to.include('C025735');
-  })
+  });
 
 });
